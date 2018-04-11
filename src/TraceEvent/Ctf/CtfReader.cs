e34a8a0bf73fa90afb9f30209e75921ba8998af4
Gregory LEOCADIE <g.leocadie@criteo.com>
2018-04-11 15:26:45 +0200

    Optimization: Avoid boxing when reading and converting CtfInteger
    This conversion is on the critical path and generate a huge amount of
    object instances which come from the boxing of integer type.
    
    By using generic + interface magic we can avoid boxing and improve the
    performance drastically.
    
    + Tests

--- src/TraceEvent/Ctf/CtfReader.cs
+++ src/TraceEvent/Ctf/CtfReader.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
+using Microsoft.Diagnostics.Tracing.Ctf.CtfMetadataTypes;
 
 namespace Microsoft.Diagnostics.Tracing.Ctf
 {
